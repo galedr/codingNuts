29ae2101d10257c25fa0f252b91a086efb05c732
galedr <galedr1985@gmail.com>
2017-03-08 12:10:45 +0800

    後台管理者登入

--- application/controllers/Page_back_end.php
+++ application/controllers/Page_back_end.php
@@ -11,6 +11,48 @@ class Page_back_end extends MY_Controller
 		$this->load->library('setsession');
 	}
 
+	public function admin_login_page()
+	{
+		$this->load->view('desktop/admin_login');
+	}
+
+	public function admin_login()
+	{
+		$admin_account = (empty($_POST['adminAccount'])) ? false : $_POST['adminAccount'];
+		$admin_password = (empty($_POST['adminPassword'])) ? false : $_POST['adminPassword'];
+
+		if ($admin_account == false or $admin_password == false) {
+			$data['error_message'] = "您的帳號與密碼皆不能為空白";
+			$data['redirect'] = base_url()."back_end/admin_login";
+
+			$this->load->view('desktop/error_page', $data);
+			return;
+		}
+
+		$this->load->Model('admin');
+
+		$result = $this->admin->check($admin_account, $admin_password);
+
+		if (count($result) == 0) {
+			$data['error_message'] = "您的帳號或密碼有誤";
+			$data['redirect'] = base_url()."back_end/admin_login";
+
+			$this->load->view('desktop/error_page', $data);
+			return;
+		}
+
+		$session_key = "codingNuts_admin";
+
+		$this->setsession->admin_set($session_key, $admin_account);
+
+		header("location:".base_url()."back_end");
+	}
+
+	public function output_list()
+	{
+
+	}
+
 }
 
 ?>
