ba7439961fd9570f486ca464664390b8a9730cd7
galedr <galedr1985@gmail.com>
2017-03-09 16:42:04 +0800

    移動檔案夾，重置base_url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/php/gitProject/codingNuts';
+$config['base_url'] = 'http://localhost/php/github/codingNuts';
 
 /*
 |--------------------------------------------------------------------------
